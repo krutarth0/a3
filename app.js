ba5da88e9e34635354cadbce28e13fe873e4cb05
Krutarth Patel <krutarthzero@hotmail.com>
2022-03-25 20:02:05 -0300

    sdfsdf

--- app.js
+++ app.js
@@ -87,6 +87,28 @@ app.get("/liststudents", (req, res) => {
   });
 });
 
+app.get("/clear", (req, res) => {
+  con.connect(function (err) {
+    if (err) console.log(err);
+    con.query("TRUNCATE TABLE a3data.students");
+  });
+});
+
+app.get("/create", (req, res) => {
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+
+    con.query("CREATE DATABASE IF NOT EXISTS a3data;");
+    con.query("USE a3data;");
+    con.query(
+      "CREATE TABLE IF NOT EXISTS students(id int NOT NULL AUTO_INCREMENT, first_name varchar(100), last_name varchar(100), banner varchar(20), PRIMARY KEY(id));",
+      function (error, result, fields) {
+        console.log(result);
+      }
+    );
+  });
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
