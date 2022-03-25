e34cd279f9b64e12a991297b541361b7df67c0fa
Krutarth Patel <krutarthzero@hotmail.com>
2022-03-24 21:26:20 -0300

    letsgo

--- db.js
+++ db.js
@@ -9,5 +9,15 @@ const con = mysql.createConnection({
 con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
+
+  con.query("CREATE DATABASE IF NOT EXISTS a3data;");
+  con.query("USE a3data;");
+  con.query(
+    "CREATE TABLE IF NOT EXISTS students(id int NOT NULL AUTO_INCREMENT, first_name varchar(100), last_name varchar(100), banner varchar(20));",
+    function (error, result, fields) {
+      console.log(result);
+    }
+  );
+
   con.end();
 });
